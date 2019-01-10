2cc95ceb1b8984dffea6307d5ecf3a5aebd690d6
Hiroshi Shimizu <igo-soccer@eis.hokudai.ac.jp>
2019-01-11 04:18:34 +0900

    second commit

---
+++ copy_sheet.py
@@ -0,0 +1,50 @@
+import openpyxl, sys
+
+if len(sys.argv) != 5:
+    print("使い方：python3 copy_sheet.py wb1 sheet1 wb2 sheet2")
+    print("コマンドライン引数は4つ:book,sheet,book,sheetの順")
+    sys.exit()
+
+args = sys.argv
+original_wb_name = args[1]
+original_sheet_name = args[2]
+copy_wb_name = args[3]
+copy_sheet_name = args[4]
+
+try:
+    original_wb = openpyxl.load_workbook(original_wb_name)
+except FileNotFoundError as e:
+    print(e)
+    exit()
+else:
+    pass
+
+try:
+    original_sheet = original_wb.get_sheet_by_name(original_sheet_name)
+except KeyError as e:
+    print(e)
+    exit()
+else:
+    pass
+
+try:
+    copy_wb = openpyxl.load_workbook(copy_wb_name)
+except FileNotFoundError as e:
+    print(e)
+    exit()
+else:
+    pass
+
+try:
+    copy_sheet = copy_wb.get_sheet_by_name(copy_sheet_name)
+except KeyError as e:
+    print(e)
+    exit()
+else:
+    pass
+
+for row_num in range(1,original_sheet.max_row + 1):
+    for column_num in range(1,original_sheet.max_column +1):
+        copy_sheet.cell(row=row_num,column=column_num).value = original_sheet.cell(row=row_num,column=column_num).value
+
+copy_wb.save(copy_wb_name)
---
+++ execute.sh
@@ -0,0 +1,2 @@
+sed -in "1 s/#    time/#____time/g" 000.dat
+sed -in "1 s/#    time/#____time/g" 000.ufm
