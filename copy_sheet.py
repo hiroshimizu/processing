18ae0104bf3e971e2429a57736d784c76206d9d3
Hiroshi Shimizu <igo-soccer@eis.hokudai.ac.jp>
2019-01-13 06:32:02 +0900

    copy_sheetを一部関数化

--- copy_sheet.py
+++ copy_sheet.py
@@ -1,56 +1,53 @@
 """
     xlsxファイルのシートを別のブックのシートにコビーする
     使い方：python3 copy_sheet.py wb1 sheet1 wb2 sheet2
-    ☆try-exceptのところ関数とかメソッドで書くべきだろ馬鹿　勉強不足
     2019/01/10 Shimizu
 """
 import openpyxl, sys
 
+#****************workbook名からworkbookオブジェクトを取得する関数get_workbook
+def get_workbook(workbook_name):
+    try:
+        workbookObj = openpyxl.load_workbook(workbook_name)
+    except FileNotFoundError as e:
+        print(e)
+        exit()
+    else:
+        return workbookObj
+
+#****************workbookオブジェクトとworksheet名からworksheetオブジェクトを取得する関数get_worksheet
+def get_worksheet(workbookObj,worksheet_name):
+    try:
+        worksheetObj = workbookObj.get_sheet_by_name(worksheet_name)
+    except KeyError as e:
+        print(e)
+        exit()
+    else:
+        return worksheetObj
+
+#*******************コマンドライン引数の数が正しいか判定********************
 if len(sys.argv) != 5:
     print("使い方：python3 copy_sheet.py wb1 sheet1 wb2 sheet2")
     print("コマンドライン引数は4つ:book,sheet,book,sheetの順")
     sys.exit()
 
+#**************************変数宣言・代入***********************************
 args = sys.argv
 original_wb_name = args[1]
 original_sheet_name = args[2]
 copy_wb_name = args[3]
 copy_sheet_name = args[4]
 
-try:
-    original_wb = openpyxl.load_workbook(original_wb_name)
-except FileNotFoundError as e:
-    print(e)
-    exit()
-else:
-    pass
+original_wb = get_workbook(original_wb_name)
+original_sheet = get_worksheet(original_wb,original_sheet_name)
+copy_wb = get_workbook(copy_wb_name)
+copy_sheet = get_worksheet(copy_wb,copy_sheet_name)
 
-try:
-    original_sheet = original_wb.get_sheet_by_name(original_sheet_name)
-except KeyError as e:
-    print(e)
-    exit()
-else:
-    pass
-
-try:
-    copy_wb = openpyxl.load_workbook(copy_wb_name)
-except FileNotFoundError as e:
-    print(e)
-    exit()
-else:
-    pass
-
-try:
-    copy_sheet = copy_wb.get_sheet_by_name(copy_sheet_name)
-except KeyError as e:
-    print(e)
-    exit()
-else:
-    pass
-
+#**************************メイン処理***************************************
 for row_num in range(1,original_sheet.max_row + 1):
     for column_num in range(1,original_sheet.max_column +1):
         copy_sheet.cell(row=row_num,column=column_num).value = original_sheet.cell(row=row_num,column=column_num).value
 
 copy_wb.save(copy_wb_name)
+
+
